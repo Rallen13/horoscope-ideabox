7e684c7bd19d445b11d993f48adcb8a3d0d1a418
Rachel Allen <rallenco13@gmail.com>
2022-07-19 11:53:40 -0600

    completed App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import React, { Component } from "react";
+import "./App.css";
 
-import './App.css';
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      horoscopes: [
+        {
+          date_range: "Apr 21 - May 20",
+          current_date: "July 19, 2022",
+          description:
+            "That shift in the atmosphere around you isn't your imagination. There's a certain something out there that's changed, and it's for the better. You may not be especially fond of change in general, but this time you'll love it.",
+          compatibility: "Aries",
+          mood: "Curious",
+          color: "Orange",
+          lucky_number: "18",
+          lucky_time: "3pm",
+        },
+      ],
+    };
+  }
 
-function App() {
-  return (
-
-  );
+  render() {
+    return (
+      <main className="App">
+        <h1>Horoscope Generator</h1>
+        <p>Taurus</p>
+      </main>
+    );
+  }
 }
 
 export default App;
