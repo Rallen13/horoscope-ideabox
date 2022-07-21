96ef0fe5da03dedc8b9809b07e0d806a24472558
Rachel Allen <rallenco13@gmail.com>
2022-07-21 09:43:02 -0600

    set up HoroscopesContainer.js and .css and injected container into app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { getHoroscope } from "./ApiCalls";
+import HoroscopesContainer from "./components/HoroscopesContainer";
 
 class App extends Component {
   constructor() {
@@ -19,18 +20,7 @@ class App extends Component {
     return (
       <main className="App">
         <h1>Generate Horoscope</h1>
-        <section className="horoscope-container">
-          <div className="horoscope-card">
-            <h2>{this.state.horoscope.date_range}</h2>
-            <h2>{this.state.horoscope.current_date}</h2>
-            <h2>{this.state.horoscope.description}</h2>
-            <h2>{this.state.horoscope.compatibility}</h2>
-            <h2>{this.state.horoscope.mood}</h2>
-            <h2>{this.state.horoscope.color}</h2>
-            <h2>{this.state.horoscope.lucky_number}</h2>
-            <h2>{this.state.horoscope.lucky_time}</h2>
-          </div>
-        </section>
+        <HoroscopesContainer horoscope={this.state.horoscope} />
       </main>
     );
   }
