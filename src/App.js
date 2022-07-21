d7bfca7e4021ec0100740a370cec012705ac5d61
Rachel Allen <rallenco13@gmail.com>
2022-07-21 09:04:58 -0600

    set up App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 import React, { Component } from "react";
-import "./App.css";
-import HoroscopesContainer from "./components/HoroscopesContainer";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      horoscopes: [
-        {
-          date_range: "Apr 21 - May 20",
-          current_date: "July 19, 2022",
-          description:
-            "That shift in the atmosphere around you isn't your imagination. There's a certain something out there that's changed, and it's for the better. You may not be especially fond of change in general, but this time you'll love it.",
-          compatibility: "Aries",
-          mood: "Curious",
-          color: "Orange",
-          lucky_number: "18",
-          lucky_time: "3pm",
-        },
-      ],
+      horoscope: {
+        date_range: "Apr 21 - May 20",
+        current_date: "July 19, 2022",
+        description:
+          "That shift in the atmosphere around you isn't your imagination. There's a certain something out there that's changed, and it's for the better. You may not be escpecially fond of change in general, but this time you will love it.",
+        compatibility: "Aries",
+        mood: "Curious",
+        color: "Orange",
+        lucky_number: "18",
+        lucky_time: "3pm",
+      },
     };
   }
 
   render() {
     return (
       <main className="App">
-        <h1>Horoscope Generator</h1>
-        <HoroscopesContainer />
+        <h1>Generate Horoscope</h1>
+        <section className="horoscope-container">
+          <div className="horoscope-card">
+            <h2>{this.state.horoscope.date_range}</h2>
+            <h2>{this.state.horoscope.current_date}</h2>
+            <h2>{this.state.horoscope.description}</h2>
+            <h2>{this.state.horoscope.compatibility}</h2>
+            <h2>{this.state.horoscope.mood}</h2>
+            <h2>{this.state.horoscope.color}</h2>
+            <h2>{this.state.horoscope.lucky_number}</h2>
+            <h2>{this.state.horoscope.lucky_time}</h2>
+          </div>
+        </section>
       </main>
     );
   }
